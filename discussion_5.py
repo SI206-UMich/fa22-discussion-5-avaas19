68e00b4356960bae54808a16e9292812b54baab4
avaas19 <avaas@umich.edu>
2022-09-28 16:02:27 -0400

    count_a() works

--- discussion_5.py
+++ discussion_5.py
@@ -3,8 +3,8 @@ import unittest
 # Counts the number of a's in a sentence (e.g., a string)
 def count_a(sentence):
 	total = 0
-	for i in range(len(sentence) - 1):
-		if i == 'a':
+	for i in sentence:
+		if i == 'a' or i == "A":
 			total += 1
 	return total
 
@@ -63,11 +63,15 @@ class TestAllMethods(unittest.TestCase):
 
 	## Check to see whether count_a works
 	def test_count_a(self):
-		pass
-
+		string = "This is All crazy"
+		self.assertEqual(count_a(string),2)
 
 	## Check to see whether you can add an item to the warehouse
 	def test_add_item(self):
+		new = Warehouse()
+		
+
+		self.assertEqual(add_item(),2)
 		pass
 
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+	# def test_count_a(self):
+	# 	self.assertEqual(count_a("This is all crazy."),2,"Tested count_a on input This is all crazy.")
+	# 	self.assertEqual(count_a("This is AAAll crazy"),4,"Tested count_a on input This is AAAll crazy")
